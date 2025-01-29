ac071dd52004d514cfc39cd1af261c7a3803be0b
Divyanshu Yadav <divycic1@gmail.com>
2025-01-30 02:51:25 +0530

    crud for user

--- src/app.js
+++ src/app.js
@@ -18,10 +18,35 @@ app.post('/signup', async (req, res) => {
     }
 });
 
-app.use("/test", (req, res) => {
-    res.send('Hello World');
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.send(users);
+    }catch(err) {
+        res.status(400).send("Error while getting users " + err.message);
+    }
 });
 
+app.patch('/users', async (req, res) => {
+    const userId = req.body.id;
+    const data = req.body;
+    try {
+        const updatedUser = await User.findByIdAndUpdate(userId, data, {returnDocument: 'after'});
+        res.send(updatedUser);
+    }catch(err) {
+        res.status(400).send("Error while updating user " + err.message);
+    }
+});
+
+app.delete('/users', async (req, res) => {
+    const userId = req.body.id;
+    try {
+        await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
+    }catch(err) {
+        res.status(400).send("Error while deleting user " + err.message);
+    }
+});
 
 
 connectDB()
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    await mongoose.connect("");
+    await mongoose.connect("mongodb+srv://divycic1:div@cluster0.gwxvg.mongodb.net/devhubdb");
 };
 
 module.exports = connectDB;
