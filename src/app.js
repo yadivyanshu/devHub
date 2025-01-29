c10a287f7125f9dbd280a1586b37257032697304
Divyanshu Yadav <divycic1@gmail.com>
2025-01-30 02:07:09 +0530

    tested first api

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const connectDB = require('./config/database');
+const User = require('./models/user');
+
+
 const app = express();
+
+app.use(express.json());
+
+app.post('/signup', async (req, res) => {
+    const user = new User(req.body);
+    console.log(user);
+    try {
+        await user.save();
+        res.send("User created successfully");
+    }catch(err) {
+        res.status(400).send("Error while creating user " + err.message);
+    }
+});
 
 app.use("/test", (req, res) => {
     res.send('Hello World');
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://divycic1:div@cluster0.gwxvg.mongodb.net/devhubdb");
+    await mongoose.connect("");
 };
 
 module.exports = connectDB;
