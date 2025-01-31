6893d6dd1efcb47855fe73c9f2c39c65be6f8f9b
Divyanshu Yadav <divycic1@gmail.com>
2025-02-01 04:37:29 +0530

    implemented pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -51,6 +51,12 @@ router.get('/user/connections', userAuth, async (req, res) => {
 router.get('/feed', userAuth, async (req, res) => {
     try {
         const loggedInUser = req.user;
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = Math.min(limit, 25);
+
+        const skip = (page - 1) * limit;
+
         const connectionRequests = await ConnectionRequest.find(
         {
             $or: [
@@ -70,7 +76,9 @@ router.get('/feed', userAuth, async (req, res) => {
                 {_id: { $nin: Array.from(hideUsersFromFeed) }},
                 {_id: { $ne: loggedInUser._id }}
             ]
-        }).select(USER_SAFE_FIELDS);
+        }).select(USER_SAFE_FIELDS)
+            .skip(skip)
+            .limit(limit);
 
         res.status(200).json({
             message: "Feed fetched successfully",
