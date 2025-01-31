e76d463e67113f12faef88395c6df0aaa8ab4daf
Divyanshu Yadav <divycic1@gmail.com>
2025-02-01 00:08:31 +0530

    indexing the db

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -19,6 +19,8 @@ const connectionRequestSchema = new schema(
     }
 );
 
+connectionRequestSchema.index({fromUserId: 1, toUserId: 1}, {unique: true});
+
 connectionRequestSchema.pre('save', function(next) {
     const connectionRequest = this;
     if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
