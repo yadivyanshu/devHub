92dbacf151b49127de77b31ee870c345223b8b5f
Divyanshu Yadav <divycic1@gmail.com>
2025-02-03 08:26:11 +0530

    temp db config update

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect('mongodb+srv://divycic1:div@cluster0.gwxvg.mongodb.net/devhubdb');
 };
 
 module.exports = connectDB;
