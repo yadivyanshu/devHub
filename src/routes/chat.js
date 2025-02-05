2a44f06e5b49b0ab0a4b3623758ad97d57f049f8
Divyanshu Yadav <divycic1@gmail.com>
2025-02-05 05:30:02 +0530

    added target user detail in /chat api response

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const { Chat } = require("../models/chat");
+const User = require("../models/user");
 
 const chatRouter = express.Router();
 
@@ -23,7 +24,12 @@ chatRouter.get("/chat/:targetUserId", userAuth, async (req, res) => {
             await chat.save();
         }
 
-        res.status(200).json(chat);
+        const targetUserDetails = await User.findById(targetUserId).select("firstName lastName");
+        const targetUserFullName = targetUserDetails.firstName + " " + targetUserDetails.lastName;
+        res.status(200).json({
+            chat,
+            targetUser: targetUserFullName
+        });
     } catch (err) {
         console.error(err);
     }
